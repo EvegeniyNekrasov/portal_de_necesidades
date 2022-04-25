dd577b00e6c37413c0c962ef9a7fada4b56aece3
Erick Brito <erick261@gmail.com>
2022-04-25 21:23:53 +0200

    services.js changed

--- Repositories/services.js
+++ Repositories/services.js
@@ -12,39 +12,6 @@ const getServices = async (req, res) => {
     }
 
 }
-const listProducts = async (req, res) => {
-    // http://localhost:4444/product?max_price=2000&min_price=1000
-    // http://localhost:4444/product?max_price=2000
-    // http://localhost:4444/product?min_price=1000
-    // http://localhost:4444/product
-    const { max_price, min_price } = req.query
-
-    const connection = await db.getConnection()
-    
-    let sql = 'select id, name, price, category from products'
-    let filters = []
-    
-    if (min_price) {
-        filters.push(`price >= ${min_price}`)
-    }
-
-    if (max_price) {
-        filters.push(`price <= ${max_price}`)
-    }
-
-    // si la longitud de filters es distinto de cero
-    // significa que nos pasaron al menos un filtro, min_price o max_price,
-    // y por tanto tenemos que añadir el where a la sentencia sql
-    if (filters.length !== 0) {
-        sql = `${sql} where ${filters.join(' and ')}`
-    }
-
-    const listOfProducts = await connection.query(sql)
-
-    let filteredProducts = listOfProducts[0]
-
-    res.send(filteredProducts)
-}
 
 const createService = async (title, describe, file, comments, userId, connection) => {
     try {
